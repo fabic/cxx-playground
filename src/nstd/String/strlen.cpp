bbcc135244b9e0e1320fa6b211c946a7f9ddb056
Fabien Cadet <fabi@winterfell.home>
2017-02-19 10:37:07 +0400

    String::length() : 1st impl. (from the book Hacker's Delight)

--- src/nstd/String/strlen.cpp
+++ src/nstd/String/strlen.cpp
@@ -1,13 +1,56 @@
 
 #include <String.hpp>
 
-#define ONES ((size_t)-1/UCHAR_MAX)
-#define HIGHS (ONES * (UCHAR_MAX/2+1))
-#define HASZERO(x) ((x)-ONES & ~(x) & HIGHS)
+// #define ONES ((size_t)-1/UCHAR_MAX)
+// #define HIGHS (ONES * (UCHAR_MAX/2+1))
+// #define HASZERO(x) ((x)-ONES & ~(x) & HIGHS)
 
 namespace nstd {
 
   using nstd::size_t;
+
+  /**
+   * <pre>
+   * ~$ python
+   *
+   * # "MAGIC" MASK
+   * > m=0x7F7F7F7F
+   *
+   * # Word `x` has no zero byte.
+   *
+   * > x=0x79efac33 ;  y=(x&m)+m; z=~(y|x|m); 'x={0:#x} | y={1:#x} | z={2:#x}'.format(x,y,z)
+   *  'x=0x79efac33 | y=0xf8eeabb2 | z=-0x100000000'
+   *                                       ~~~~~~~~ 4 bytes of word turned zero.
+   *                                      ^ overflow.
+   *
+   * # Else the 1st least-significant byte that is zero
+   * # turn 0x80 with leading bytes at 0x00.
+   *
+   * > x=0x00efac33 ; y=(x&m)+m; z=~(y|x|m); 'x={0:#x} |  y={1:#x} | z={2:#x}'.format(x,y,z)
+   * 'x=0x00efac33 | y=0x7feeabb2 | z=-0x80000000'
+   *
+   * > x=0x7900ac33 ; y=(x&m)+m; z=~(y|x|m); 'x={0:#x} | y={1:#x} | z={2:#x}'.format(x,y,z)
+   * 'x=0x7900ac33 | y=0xf87fabb2 | z=-0xff800000'
+   *
+   * > x=0x79ef0033 ; y=(x&m)+m; z=~(y|x|m); 'x={0:#x} | y={1:#x} | z={2:#x}'.format(x,y,z)
+   * 'x=0x79ef0033 | y=0xf8ee7fb2 | z=-0xffff8000'
+   *
+   * > x=0x79efac00 ; y=(x&m)+m; z=~(y|x|m); 'x={0:#x} | y={1:#x} | z={2:#x}'.format(x,y,z)
+   * 'x=0x79efac00 | y=0xf8eeab7f | z=-0xffffff80'
+   * </pre>
+   */
+  inline bool word_has_zero(unsigned long x)
+  {
+    using word_t = decltype(x);
+
+    constexpr word_t m = 0x7F7F7F7FL | (0x7F7F7F7FL << 32);
+
+    word_t y = (x & m) + m;
+
+    word_t z = ~(y | x | m);
+
+    return z != 0;
+  }
 
   /**
    * Borrowed from musl-libc `src/string/strlen.c`
@@ -16,14 +59,24 @@ namespace nstd {
    */
   size_t String::length(const char *s)
   {
-    constexpr size_t Alignment = sizeof(size_t);
+    using word_t = const unsigned long;
+
+    constexpr size_t Alignment = sizeof(word_t);
 
     const char *a = s;
-    const size_t *w;
 
-    for (; static_cast<size_t>(s) % Alignment; s++) if (!*s) return s-a;
-    for (w = (const void *)s; !HASZERO(*w); w++);
-    for (s = (const void *)w; *s; s++);
+    for (; reinterpret_cast<word_t>(s) % Alignment; s++) {
+      if (!*s)
+        return s-a;
+    }
+
+    word_t *w = reinterpret_cast<word_t *>( s );
+
+    for (; !word_has_zero(*w); w++) {}
+
+    s = reinterpret_cast<const char *>(w);
+
+    for (; *s; s++) {}
 
     return s-a;
   }
