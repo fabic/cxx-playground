2bce33e1d06b862ae73a4a33171130f87549c1f0
Fabien Cadet <fabi@winterfell.home>
2017-02-22 13:59:18 +0400

    New project “Dumbster”

---
+++ src/dumbster/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  using namespace std;
+
+  cout << "Hey, I'm " << argv[0] << endl;
+
+  cout << "Good bye..." << endl << endl;
+
+  return 0;
+}
