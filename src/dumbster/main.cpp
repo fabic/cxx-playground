a8f736ec85cdc872a3b8829456846e925aad63f0
Fabien Cadet <fabi@winterfell.home>
2017-02-28 07:42:08 +0400

    sunny day, woke up early this morning, waiting for another build of Clang to complete

--- src/dumbster/main.cpp
+++ src/dumbster/main.cpp
@@ -2,28 +2,68 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <memory>
 
 namespace psr {
 
+
+  /**
+   */
   struct Token {
-    char character;
-    union {
       unsigned int is_eof:1;
+      unsigned int is_blank_space:1;
       unsigned int is_sharp:1;
-      unsigned int _word;
-    } kind;
-    Token(char ch);
+      unsigned int is_double_colon:1;
+      //unsigned int _unused:XXX;
+    int _start_line, _end_line;
+    int _start_col, _end_col;
+    Token() : is_eof(false), is_blank_space(false), is_sharp(false), is_double_colon(false) {}
   };
 
+
+  /**
+   */
   class Parser {
   protected:
   protected:
     std::ifstream file;
+    int _line = 1, _column = 0;
   public:
     void parseFile(std::string fileName);
     void beginParseFile();
     Token readToken();
   };
+
+
+  /**
+   */
+  struct Cursor {
+    int _lineStart = -1, _lineEnd = -1;
+    int _columnStart = -1, _columnEnd = -1;
+  };
+
+
+  /**
+   */
+  class Fragment {
+    protected:
+      std::shared_ptr<Fragment> previous_;
+      std::shared_ptr<Fragment> next_;
+      unsigned int _line, _colunm;
+      // list of lexed tokens with no semantics ?
+  };
+
+
+  /**
+   */
+  class Block : public Fragment {
+    protected:
+      std::shared_ptr<Fragment> inner_;
+  };
+
+
+  // ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~
+
 
   void Parser::parseFile(std::string fileName)
   {
@@ -43,39 +83,101 @@ namespace psr {
     std::cout << "Bye! " << fileName << std::endl;
   }
 
+
   void Parser::beginParseFile()
   {
-    auto token = readToken();
+    Token token;
+
+    while(! (token = readToken()).is_eof) {
+      std::cout << "Read token" << std::endl;
+    };
   }
 
-  Token::Token(char ch) : character(ch)
-  {
-    switch(ch) {
-    case '#':
-      kind.is_sharp = true;
-      break;
-    }
+
+
+
+  inline bool is_blank_character(char ch) {
+    return ch == ' ' || ch == '\t' || ch == '\n';
   }
 
-  Token Parser::readToken()
+
+  Token Parser::readToken() // todo: ren. nextToken().
   {
-    Token tok(-1);
+    Token token;
 
     char ch = file.get();
 
-    if (file.eof()) {
-      tok.kind.is_eof = true;
-      return tok;
+    bool reached_eof = file.eof();
+
+    if (reached_eof) {
+      token.is_eof = true;
+      return token;
     }
 
+    _column++;
 
+    // Consume a block of blank space.
+    if (is_blank_character(ch))
+    {
+      token._start_line = _line;
+      token._start_col  = _column;
 
-    return tok;
+      reached_eof = false;
+
+      while(is_blank_character((ch = file.get())))
+      {
+        reached_eof = file.eof();
+
+        if (reached_eof) {
+          break;
+        }
+        else if (ch == '\n') {
+          _line++;
+          _column = 0;
+          continue;
+        }
+
+        _column++;
+      }
+
+      if (! reached_eof)
+        file.putback(ch);
+
+      token._end_line = _line;
+      token._end_col  = _column;
+
+      return token;
+    }
+
+    // Consume whatever stuff we do not know about.
+    token._start_line = _line;
+    token._start_col  = _column;
+
+    while(! is_blank_character((ch = file.get())))
+    {
+      reached_eof = file.eof();
+
+      if (reached_eof) {
+        break;
+      }
+
+      _column++;
+    }
+
+    if (! reached_eof)
+      file.putback(ch);
+
+    token._end_line = _line;
+    token._end_col  = _column;
+
+    return token;
   }
 
 } // psr ns.
 
 
+/**
+ */
 int main(int argc, const char *argv[])
 {
   using namespace std;
